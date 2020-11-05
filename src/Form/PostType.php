1113afb0ebc50964d49ba0be528205fd84af94c0
Jason Monroe <jason@jasonmonroe.com>
2020-11-05 01:13:12 -0600

    manually adding tmp2 branch to main

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -6,16 +6,19 @@ use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('body')
-           // ->add('user_id')
-        ;
+            ->add('title', TextType::class, ['attr' => ['class' => 'form-control', 'maxlength' => 32]])
+            ->add('body', TextAreaType::class,
+                ['attr' => ['class' => 'form-control', 'cols' => 4, 'maxlength' =>255]]
+            );
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
